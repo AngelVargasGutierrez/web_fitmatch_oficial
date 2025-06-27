9c2835c2309238ad878d81b423ea29937ca624b7
Angel Jose Vargas Gutierrez <av923488264@gmail.com>
2025-06-27 14:01:28 -0500

    BD23

---
+++ public/mensajes.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../app/controllers/UserController.php';
+$userController = new UserController();
+if (!$userController->isLoggedIn()) {
+    header('Location: login.php');
+    exit;
+}
+$currentUser = $userController->getCurrentUser();
+include __DIR__ . '/../app/views/mensajes.php'; 
